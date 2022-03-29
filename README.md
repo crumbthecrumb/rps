45fee5a234b26bbd9833952245c4feea87b0e20f
crumbthecrumb <102608434+crumbthecrumb@users.noreply.github.com>
2022-03-29 17:08:31 +0200

    Initiala filer för MakeCode-projekt

---
+++ README.md
@@ -0,0 +1,31 @@
+
+> Öppna denna sida på [https://crumbthecrumb.github.io/rps/](https://crumbthecrumb.github.io/rps/)
+
+## Använd som tillägg
+
+Denna databas kan läggas till som ett **tillägg** i MakeCode.
+
+* öppna [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* klicka på **Nytt projekt**
+* klicka på **Tillägg** under kugghjulsmenyn
+* sök efter **https://github.com/crumbthecrumb/rps** och importera
+
+## Redigera detta projekt ![Byggets statusikon](https://github.com/crumbthecrumb/rps/workflows/MakeCode/badge.svg)
+
+Redigera den här databasen i MakeCode.
+
+* öppna [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* klicka på **Importera** och klicka sedan på **Importera URL**
+* klistra in **https://github.com/crumbthecrumb/rps** och klicka på importera
+
+## Förhandsgranskning av block
+
+Denna bild visar blockkoden från den senaste incheckningen i master.
+Denna bild kan ta några minuter att ladda om.
+
+![En renderad vy av blocken](https://github.com/crumbthecrumb/rps/raw/master/.github/makecode/blocks.png)
+
+#### Metadata (används för sökning och rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,117 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    svar_2 = receivedNumber
+})
+function restart () {
+    go = 0
+    svar = 0
+    svar_2 = 0
+    basic.clearScreen()
+}
+input.onButtonPressed(Button.A, function () {
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    if (go == 0) {
+        svar = 1
+        radio.sendNumber(1)
+        basic.showLeds(`
+            . . . . .
+            . # # . .
+            # # # # #
+            # # # # #
+            . # # # .
+            `)
+    } else {
+        restart()
+        radio.sendString("")
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    if (go == 0) {
+        svar = 3
+        radio.sendNumber(3)
+        basic.showLeds(`
+            . # . . .
+            # # . . #
+            # . . . #
+            # . . . #
+            . # # # .
+            `)
+    } else {
+        restart()
+        radio.sendString("")
+    }
+})
+radio.onReceivedString(function (receivedString) {
+    restart()
+})
+input.onButtonPressed(Button.B, function () {
+    music.playTone(262, music.beat(BeatFraction.Quarter))
+    if (go == 0) {
+        svar = 2
+        radio.sendNumber(2)
+        basic.showIcon(IconNames.Scissors)
+    } else {
+        restart()
+        radio.sendString("")
+    }
+})
+let svar = 0
+let go = 0
+let svar_2 = 0
+radio.setGroup(88)
+radio.setTransmitPower(1)
+restart()
+basic.forever(function () {
+    if (svar_2 > 0 && svar > 0) {
+        radio.sendNumber(svar)
+        go = 1
+        basic.pause(500)
+        basic.clearScreen()
+        if (svar == 1) {
+            led.plot(0, 2)
+            led.plot(0, 1)
+            led.plot(1, 2)
+            led.plot(1, 1)
+        } else if (svar == 2) {
+            led.plot(0, 2)
+            led.plot(1, 1)
+        } else {
+            led.plot(0, 2)
+            led.plot(0, 1)
+            led.plot(1, 2)
+        }
+        if (svar_2 == 1) {
+            led.plot(3, 2)
+            led.plot(3, 1)
+            led.plot(4, 2)
+            led.plot(4, 1)
+        } else if (svar_2 == 2) {
+            led.plot(4, 2)
+            led.plot(3, 1)
+        } else {
+            led.plot(3, 2)
+            led.plot(4, 1)
+            led.plot(4, 2)
+        }
+        music.playMelody("C C5 - - - - - - ", 360)
+        basic.pause(2000)
+        if (svar == svar_2) {
+            basic.showLeds(`
+                . . . . .
+                . # . # .
+                . . . . .
+                # # # # #
+                . . . . .
+                `)
+            music.playMelody("E B C5 A B G A G ", 360)
+        } else if (svar == 1 && svar_2 == 2 || svar == 2 && svar_2 == 3 || svar == 3 && svar_2 == 1) {
+            basic.showIcon(IconNames.Happy)
+            music.playMelody("G B A G C5 B A C5 ", 360)
+        } else {
+            basic.showIcon(IconNames.Sad)
+            music.playMelody("G F G A - F E D ", 360)
+        }
+        basic.pause(2000)
+        restart()
+    }
+})
